14a141305d6f5222ba14130ad9462d3d093246dd
schmidtfx <felix.schmidt@oracle.com>
2015-04-10 13:09:16 +0100

    Fixes #1 and adds a python sensor which fetches openweathermap.org periodically

--- sensors/weather.py
+++ sensors/weather.py
@@ -1,5 +1,7 @@
 import urllib2
 import ConfigParser
+import json
+import time
 
 config = ConfigParser.ConfigParser()
 config.readfp(open('weather.cfg'))
@@ -9,7 +11,24 @@ address=config.get("Sensor", "url")
 url="%s?%s" % (address, city)
 print url
 
-response = urllib2.urlopen(url).read()
-print response
+sensor_id = str(config.get("Sensor", "id"))
+freq = int(config.get("Sensor", "freq"))
 
+print "Sensor id: %s - sampling frequency: %d" % (sensor_id, freq)
 
+while True:
+  weather_data = json.loads(urllib2.urlopen(url).read())
+  temperature = weather_data["main"]["temp"]
+
+  msg = {
+    "sensor" : {
+      "id" : sensor_id
+    },
+    "time" : int(time.time() * 1000),
+    "value" : temperature - 273.15
+  }
+
+  producer.send_messages("sensorstream", json.dumps(msg))
+
+  print msg
+  time.sleep(freq)
