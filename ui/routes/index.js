1c2f0bd215dc8010afae4c358770f0d44f1b10d9
schmidtfx <schmidtfx@gmail.com>
2015-04-13 16:10:21 +0100

    Fix typo

--- ui/routes/index.js
+++ ui/routes/index.js
@@ -7,19 +7,19 @@ var db = new sqlite3.Database('/home/pi/homeauto/backend/temp.db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  revision.short(function(short)) {
+  revision.short(function(short) {
     res.render('index', { title: 'Temperature', page: 'home' , commit: short });
   });
 });
 
 router.get('/temperature', function(req, res, next) {
-  revision.short(function(short)) {
+  revision.short(function(short) {
     res.render('temperature', { title: 'Temperature', page: 'temperature', commit: short });
   });
 });
 
 router.get('/sensorstream', function(req, res, next) {
-  revision.short(function(short)) {
+  revision.short(function(short) {
     res.render('sensorstream', { title: 'Sensorstream', page: 'sensorstream', commit: short });
   });
 });
