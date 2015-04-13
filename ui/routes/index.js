3caece89f83af27c9650a854397497b188dce793
schmidtfx <schmidtfx@gmail.com>
2015-04-13 16:08:26 +0100

    Add revision to footer

--- ui/routes/index.js
+++ ui/routes/index.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var revision = require('git-rev');
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('/home/pi/homeauto/backend/temp.db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Temperature', page: 'home' });
+  revision.short(function(short)) {
+    res.render('index', { title: 'Temperature', page: 'home' , commit: short });
+  });
 });
 
 router.get('/temperature', function(req, res, next) {
-  res.render('temperature', { title: 'Temperature', page: 'temperature' });
+  revision.short(function(short)) {
+    res.render('temperature', { title: 'Temperature', page: 'temperature', commit: short });
+  });
 });
 
 router.get('/sensorstream', function(req, res, next) {
-  res.render('sensorstream', { title: 'Sensorstream', page: 'sensorstream' });
+  revision.short(function(short)) {
+    res.render('sensorstream', { title: 'Sensorstream', page: 'sensorstream', commit: short });
+  });
 });
 
 module.exports = router;
